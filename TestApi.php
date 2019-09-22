40027cb4bcd38dee78144c4f97b64a1814208e02
ncheref <cheref.naim@gmail.com>
2019-09-22 22:02:24 +0200

    Test insert API

--- TestApi.php
+++ TestApi.php
@@ -1,9 +1,32 @@
 <?php
+
+############ TEST INSERT ############
+$post = [
+    'title'         => 'new advert',
+    'content'       => 'Content of advert',
+    'category'      => 'Immobilier',
+    'area'          => 100,
+    'price'         => 1000,
+];
+
+$ch = curl_init('http://localhost:8080/leboncoin/web/app_dev.php/adverts/create');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+// execute!
+$response = curl_exec($ch);
+
+// close the connection, release resources used
+curl_close($ch);
+
+
+############ TEST SHOW ############
+
 // create curl resource
 $ch = curl_init();
 
 // set url
-curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/leboncoin/web/app_dev.php/adverts/show/2");
+curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/leboncoin/web/app_dev.php/adverts/show/1");
 
 //return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
